82770fa773af60c864ecefee59820ac0f25154a2
haru-labo <harukun.space@gmail.com>
2019-02-27 22:30:05 +0900

    add time control button function

--- js/timer.js
+++ js/timer.js
@@ -1,26 +1,79 @@
 let countdownTimer;
+//デフォルト値
 let defMinute = 0;
-let defSeconds = 5;
+let defSeconds = 0;
+let defColor = 'black'
 let defInnerHTML;
+//ユーザー設定時刻
+let setMinute = defMinute;
+let setSeconds = defSeconds;
+
+//////////各ボタン処理//////////
 
 //スタート
 function start() {
+  //タイマー開始
   countdownTimer = setInterval(countdown, 1000);
+  document.getElementById('btnStart').disabled = "true";
 }
 
 //ストップ
 function stop() {
   clearInterval(countdownTimer);
+  document.getElementById('btnStart').disabled = "";
 }
 
 //リセット
 function reset() {
   clearInterval(countdownTimer);
+  setMinute = defMinute;
+  setSeconds = defSeconds;
   if(defInnerHTML !== undefined) {
     document.getElementById('time').innerHTML = defInnerHTML;
+    document.getElementById('time').style.color = defColor;
   }
   display(defMinute,defSeconds);
+  document.getElementById('btnStart').disabled = "";
 }
+
+//+1Min
+function plusMin() {
+  if(setMinute === 99) {
+    setMinute = 0;
+  } else {
+    setMinute++;
+  }
+  display(setMinute, setSeconds);
+}
+
+//-1Min
+function minusMin() {
+  if(setMinute > 0) {
+    setMinute--;
+    display(setMinute, setSeconds);
+  }
+}
+
+//+1Sec
+function plusSec() {
+  if(setSeconds === 99) {
+    setSeconds = 0;
+  } else {
+    setSeconds++;
+  }
+  display(setMinute, setSeconds);
+}
+
+//-1Sec
+function minusSec() {
+  if(setSeconds > 0) {
+    setSeconds--;
+    display(setMinute, setSeconds);
+  }
+}
+
+
+//////////画面制御//////////
 
 //カウントダウン処理
 function countdown() {
@@ -28,12 +81,9 @@ function countdown() {
   let numMinute = Number(document.getElementById('minute').textContent);
   let numSeconds = Number(document.getElementById('seconds').textContent);
   //Time Up
-  if(numMinute === 0 && numSeconds === 1) {
+  if(numMinute === 0 && numSeconds <= 1) {
     clearInterval(countdownTimer);
-    //デフォルトのHTMLを取得
-    defInnerHTML = document.getElementById('time').innerHTML;
-    document.getElementById('time').textContent = 'Time Up';
-    document.getElementById('time').style.color = 'red';
+    timeUp();
   //minute減少
   } else if (numMinute > 0 && numSeconds === 0) {
     numMinute--;
@@ -44,6 +94,14 @@ function countdown() {
     numSeconds--;
     display(numMinute,numSeconds);
   }
+}
+
+//TimeUp処理
+function timeUp() {
+  //デフォルトのHTMLを取得
+  defInnerHTML = document.getElementById('time').innerHTML;
+  document.getElementById('time').textContent = 'Time Up';
+  document.getElementById('time').style.color = 'red';
 }
 
 //画面描画
@@ -62,6 +120,9 @@ function display(numMinute,numSeconds) {
   }
 }
 
+
+//////////画面読み込み時//////////
+
 window.onload = function() {
   //デフォルト値をセット
   display(defMinute,defSeconds);
@@ -69,4 +130,8 @@ window.onload = function() {
   document.getElementById('btnStart').addEventListener("click", start, false);
   document.getElementById('btnStop').addEventListener("click", stop, false);
   document.getElementById('btnReset').addEventListener("click", reset, false);
+  document.getElementById('btnPlusMin').addEventListener("click", plusMin, false);
+  document.getElementById('btnMinusMin').addEventListener("click", minusMin, false);
+  document.getElementById('btnPlusSec').addEventListener("click", plusSec, false);
+  document.getElementById('btnMinusSec').addEventListener("click", minusSec, false);
   }
